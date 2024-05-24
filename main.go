c3ec3faf688cda65582a1dcdfe7d0c360c949b21
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-05-24 14:28:28 +0200

    Request first todo

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package InvestecAPIGo
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func TestInvestec() {
 	fmt.Println("Hello Investec API v0.0.1")
+}
+
+func GetToken() {
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+
+	if err != nil {
+    log.Fatal(err)
+  }
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(string(body))
 }
